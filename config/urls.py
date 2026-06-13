5d7b834a3b5522ad4c26e4939b8e27950d2ec793
Jules000 <45807409+Jules000@users.noreply.github.com>
2026-06-13 10:31:31 +0100

    fix: serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -35,9 +35,9 @@ urlpatterns += i18n_patterns(
     prefix_default_language=True,
 )
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        urlpatterns += [
-            path("__debug__/", include("debug_toolbar.urls")),
-        ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
